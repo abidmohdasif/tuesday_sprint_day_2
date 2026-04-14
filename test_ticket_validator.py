dadaae4289cba14d38a56585a16121fa02036401
abidmohdasif <abidmohdasif@gmail.com>
2026-04-14 12:00:02 +0000

    8 errors appeared for tests

--- test_ticket_validator.py
+++ test_ticket_validator.py
@@ -11,16 +11,16 @@ def test_validate_ticket_string(code):
         validate_ticket(12357367)
 
 def test_get_ticket_tier_valid_General(code):
-    assert get_ticket_tier("TK345678") = "General"
+    assert get_ticket_tier("TK345678") is "General"
 
 def test_get_ticket_tier_valid_VIP(code):
-    assert get_ticket_tier("TK567812") = "VIP"
+    assert get_ticket_tier("TK567812") is "VIP"
 
 def test_get_ticket_tier_valid_Plat(code):
-    assert get_ticket_tier("TK781234") = "Platinum"
+    assert get_ticket_tier("TK781234") is "Platinum"
 
 def test_calculate_total(prices,discount):
-    assert calculate_total([10,20,30],0) = 60.0
+    assert calculate_total([10,20,30],0) is 60.0
 
 def test_calculate_total_invalid_input(prices,discount):
     with pytest.raises(ValueError):
