3e6c44adc8d0d5b2c228441e7bd663b6f16baf5f
abidmohdasif <abidmohdasif@gmail.com>
2026-04-14 12:09:18 +0000

    working on it

--- ticket_validator.py
+++ ticket_validator.py
@@ -1,8 +1,16 @@
 def validate_ticket(code):
-    pass
+    if len(code) != 8:
+        return False
 
 def get_ticket_tier(code):
-    pass
+    num = int(code[3])
+    
+    if 0 <= num <= 3:
+        return "General"
+    elif 4 <= num <= 6:
+        return "VIP"
+    else:
+        return "Platinum"
 
 def calculate_total(prices,discount=0):
     pass
